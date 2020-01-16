06547855c10dd9719e9e0bd5a5ce3927d5dad1bd
Dang Duy Nam <dangduynam95@gmail.com>
2020-01-16 10:04:46 +0700

    init commit

---
+++ PipelinePicker/PipelinePicker.h
@@ -0,0 +1,19 @@
+//
+//  PipelinePicker.h
+//  PipelinePicker
+//
+//  Created by macOS on 1/16/20.
+//  Copyright © 2020 nam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PipelinePicker.
+FOUNDATION_EXPORT double PipelinePickerVersionNumber;
+
+//! Project version string for PipelinePicker.
+FOUNDATION_EXPORT const unsigned char PipelinePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PipelinePicker/PublicHeader.h>
+
+
